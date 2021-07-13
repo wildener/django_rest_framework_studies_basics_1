b1bb8842af27091bad2cd8e5e0a102d79e37d5c1
wildener <wildener@users.noreply.github.com>
2021-07-13 05:50:39 -0300

    Initial commit

---
+++ escola/views.py
@@ -0,0 +1,8 @@
+from django.http import JsonResponse
+
+
+# Create your views here.
+def alunos(request):
+    if request.method == 'GET':
+        aluno = {'id': 1, 'nome': 'Wildener'}
+        return JsonResponse(aluno)
